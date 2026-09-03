26908a7f1c05b5d8e4770a277de531a7a86ea467
Samuel Malec <malec293@gmail.com>
2026-09-03 20:35:14 +0200

    refactor index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,9 +13,6 @@ function HomepageHeader() {
         <Heading as="h1" className={styles.title}>
           Hello there 🐙
         </Heading>
-        <p className={styles.subtitle}>
-          This is my personal blog - all opinions provided here are my own.
-        </p>
         <div className={styles.buttons}>
           <Link className="button button--primary button--lg" to="/blog">
             Posts
@@ -38,12 +35,16 @@ export default function Home(): ReactNode {
           <div className="container">
             <div className={styles.card}>
               <Heading as="h2">Whoami</Heading>
-              <p>
-                I'm Sam, a 23 year old computer science student at FI MUNI in Brno.
-                Ever since I started programming, I've wanted to write my own programming language.
+              <p>I'm Sam, a 23 year old computer science student at FI MUNI in Brno.</p>    
+                <p>Ever since I started programming, I've wanted to write my own programming language.
                 Back then, the idea of creating a language seemed really cool to me.
                 I guess this curiosity eventually led me to explore compiler development, programming language design, and static analysis.
+                </p>
+                <p>
                 This blog is a place where I share notes, ideas, and things I learn along the way that I think others might find interesting.
+                </p>
+                <p>
+                <i>All opinions presented here are my personal opinions and do not reflect the opinion of my employer.</i>
               </p>
             </div>
           </div>
